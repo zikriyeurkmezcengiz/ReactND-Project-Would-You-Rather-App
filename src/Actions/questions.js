9e2ee26f87e6c26e27fbd71ce8520343198e8b9f
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-11-29 09:45:47 +0300

    add answer, addquestion reducers and  actions

--- src/Actions/questions.js
+++ src/Actions/questions.js
@@ -8,3 +8,25 @@ export function receiveQuestions(questions) {
     questions,
   };
 }
+
+export function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  };
+}
+
+export function receiveQuestions(questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  };
+}
+export function answerQuestion(authedUser, qid, answer) {
+  return {
+    type: ANSWER_QUESTION,
+    authedUser,
+    qid,
+    answer,
+  };
+}
--- src/Reducers/questions.js
+++ src/Reducers/questions.js
@@ -7,6 +7,26 @@ export default function questions(state = {}, action) {
         ...state,
         ...action.questions,
       };
+    case ADD_QUESTION:
+      const { question } = action;
+
+      return {
+        ...state,
+        [question.id]: question,
+      };
+    case ANSWER_QUESTION:
+      return {
+        ...state,
+        [action.qid]: {
+          ...state[action.qid],
+          [action.answer]: {
+            ...state[action.qid][action.answer],
+            votes: state[action.qid][action.answer].votes.concat(
+              action.authedUser
+            ),
+          },
+        },
+      };
     default:
       return state;
   }
